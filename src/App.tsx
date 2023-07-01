f0f0a342060e82ffbea9ad3145bc2f67fbabb2f2
Pablo <49590991+PabloVarg@users.noreply.github.com>
2023-07-01 16:21:05 -0600

    fix(Home): Remove routers

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home/Home";
 
@@ -6,9 +5,7 @@ function App() {
   return (
     <div className="App">
       <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
+        <Home />
       </Layout>
     </div>
   );
